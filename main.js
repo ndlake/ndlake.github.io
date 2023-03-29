39316b812d568c5e4259a4f9186c8c11306c7c40
ndlake <89551861+ndlake@users.noreply.github.com>
2023-03-28 20:20:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import * as tts from './tts.js';
+
 let url = null;
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 pdfjsLib = window['pdfjs-dist/build/pdf'];
@@ -13,10 +15,22 @@ let OGscale = 1;//being updated in renderPage
 const canvas = document.querySelector('#pdf-render');
 const container = document.getElementById("pageContainer");
 const eventBus = new pdfjsViewer.EventBus();
+
+function activateTextListeners() {
+    console.log("activate!");
+
+    let textElements = document.querySelectorAll("span[dir='ltr']");
+    console.log(textElements.length);
+    textElements.forEach((e) => {
+        e.addEventListener("click", tts.audioPlayer);
+        e.classList.add("textObject");
+        //console.log(e.textContent);
+    });
+}
+
 //render page
 function renderPage(num) {
     pageRendering = true;
-    let scale = 1;
     let renderTask = pdfDoc.getPage(pageNum).then(function(page) {
         // Associate the actual page with the view, and draw it.
         pdfPageView.setPdfPage(page);
@@ -29,9 +43,9 @@ function renderPage(num) {
         pageNumPending = null;
     }
     
-    //update page counter
+    //update page counters
     document.getElementById('page-num').textContent = num;
-
+    document.getElementById('go-to-number').placeholder = num;
 }
 
 function queueRenderPage(num) {
@@ -49,6 +63,7 @@ function onPrevPage() {
     pageNum--;
     queueRenderPage(pageNum);
 }
+
 document.getElementById('prev-page').addEventListener('click', onPrevPage);
 
 function onNextPage() {
@@ -60,6 +75,21 @@ function onNextPage() {
 }
 document.getElementById('next-page').addEventListener('click', onNextPage);
 
+function loadInputPage() {
+    let pageInputElement = document.getElementById("go-to-number");
+    if (pdfDoc === null||pageNum > pdfDoc.numPages ) {
+        return;
+    }
+    if (pageInputElement.value !== "" && Number(pageInputElement.value) <= pdfDoc.numPages) {
+        pageNum = Number(pageInputElement.value);
+        queueRenderPage(pageNum);
+        pageInputElement.value = "";
+        pageInputElement.placeholder = pageNum;
+    }
+}
+
+document.getElementById('go-to-page').addEventListener('click', loadInputPage);
+
 function resetPDF() {
     if (pdfDoc !== null) {
         pdfDoc.destroy();
@@ -69,7 +99,7 @@ function resetPDF() {
         pdfPageView.destroy();
         pdfPageView = null;
     }
-    pageDiv = document.querySelector(".page");
+    let pageDiv = document.querySelector(".page");
     if (pageDiv !== null) {
         pageDiv.remove();
     }
@@ -82,8 +112,8 @@ function resetPDF() {
     pageNumPending = null;
 }
 
-const inputElement = document.getElementById("file-selector");
-inputElement.addEventListener("change", selectedInput);
+const fileInputElement = document.getElementById("file-selector");
+fileInputElement.addEventListener("change", selectedInput);
 function selectedInput(e) {
     const fileList = e.target.files;
     if (fileList.length > 0) {
@@ -92,7 +122,9 @@ function selectedInput(e) {
         pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
             pdfDoc = pdfDoc_;
             document.getElementById('page-count').textContent = pdfDoc.numPages;
+            document.getElementById('num-pages').textContent = pdfDoc.numPages;
             let scale = 1;
+            eventBus.on("textlayerrendered", activateTextListeners);
             let pdfPage = pdfDoc.getPage(pageNum).then(function(page) {
                 let viewport = page.getViewport({scale:OGscale});
                 scale = container.clientWidth/viewport.width;
@@ -103,12 +135,12 @@ function selectedInput(e) {
                     scale: scale,
                     defaultViewport: page.getViewport({ scale: scale }),
                     eventBus
-                  });
-                  // Associate the actual page with the view, and draw it.
-                  pdfPageView.setPdfPage(page);
-                  return pdfPageView.draw();
+                    });
+                    
+                // Associate the actual page with the view, and draw it.
+                renderPage(pageNum);
             });
-        document.getElementById('page-num').textContent = pageNum;
+            document.getElementById('page-num').textContent = pageNum;
         }, function(err) {
             // show there's been an error
             const div = document.createElement('div');
